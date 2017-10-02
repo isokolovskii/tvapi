18af4d78f46f61616fdf54c2250b4c5f279402a1
Sokolovskiy Ivan(iMac) <ivan3177@gmail.com>
2017-10-02 16:34:12 +0300

    Add token validation Content property of response is now lowercase Renamed methods to token specified Cipher and secret key is now public object in Paginator class

--- resources/paginator.py
+++ resources/paginator.py
@@ -1,5 +1,6 @@
 import base64
 
+import binascii
 import webargs
 from flask import json
 from flask_restful import marshal, abort
@@ -9,14 +10,33 @@ from Crypto.Cipher import AES
 from database import session
 
 
+def check_token(token):
+    if token is None:
+        return True
+    if token == "":
+        raise webargs.ValidationError("Empty page token")
+    try:
+        decoded = Paginator.cipher.decrypt(base64.b64decode(token))
+    except binascii.Error:
+        raise webargs.ValidationError("Wrong page token")
+    try:
+        d = json.loads(decoded)
+    except ValueError:
+        raise webargs.ValidationError('Wrong page token')
+    if type(d) is not type({}):
+        raise webargs.ValidationError('Wrong page token')
+
+
 class Paginator:
     args = {
         'maxResults': webargs.fields.Int(required=False, missing=20,
-                                         validate=Range(min=1, max=100)),
-        'pageToken': webargs.fields.Str(required=False, missing=None)
+                                         validate=Range(min=1, max=99)),
+        'pageToken': webargs.fields.Str(required=False, missing=None,
+                                        validate=check_token)
     }
 
-    __secret = 'mysixteenbytekey'
+    secret = 'mysixteenbytekey'
+    cipher = AES.new(secret, AES.MODE_ECB)
 
     def get_paginated_list(self, model, maxResults, table_schema, pageToken):
         # get request and page info, check if page exists
@@ -24,24 +44,24 @@ class Paginator:
         maxResults = maxResults
         total = session.query(model.id).count()
         if pageToken is not None:
-            token_string = self.decode(pageToken)
+            token_string = self.__parse_token(pageToken)
             d = json.loads(token_string)
             start = d['start']
             maxResults = d['maxResults']
         if start > total:
             abort(404)
 
-        # generate next and previous page
+        # generate next and previous page tokens
         prevPage = ''
         nextPage = ''
         if start + maxResults <= total:
             d = {'start': start + maxResults, 'maxResults': maxResults}
             token_string = json.dumps(d)
-            nextPage = self.encode(token_string)
+            nextPage = self.__generate_token(token_string)
         if start - maxResults >= 0:
             d = {'start': start - maxResults, 'maxResults': maxResults}
             token_string = json.dumps(d)
-            prevPage = self.encode(token_string)
+            prevPage = self.__generate_token(token_string)
 
         # query items from table
         q = session.query(model).limit(maxResults)
@@ -51,8 +71,8 @@ class Paginator:
 
         # make response
         obj = {
-            'content': model.__name__,
-            'results': maxResults,
+            'content': model.__name__.lower(),
+            'results': min(maxResults, len(items)),
             'total': total,
             'prevPage': prevPage,
             'nextPage:': nextPage,
@@ -60,12 +80,10 @@ class Paginator:
         }
         return obj
 
-    def encode(self, plaintext):
-        cipher = AES.new(self.__secret, AES.MODE_ECB)
-        encoded = base64.b64encode(cipher.encrypt(plaintext.rjust(32)))
-        return str(encoded.decode('utf-8'))
+    def __generate_token(self, token_string):
+        encoded = base64.b64encode(self.cipher.encrypt(token_string.rjust(32)))
+        return encoded.decode('utf-8')
 
-    def decode(self, ciphertext):
-        cipher = AES.new(self.__secret, AES.MODE_ECB)
-        decoded = cipher.decrypt(base64.b64decode(ciphertext))
+    def __parse_token(self, token):
+        decoded = self.cipher.decrypt(base64.b64decode(token))
         return decoded.strip()
--- tvrestapi.py
+++ tvrestapi.py
@@ -13,4 +13,4 @@ api.add_resource(CategoriesResources, '/api/categories')
 
 
 if __name__ == '__main__':
-    app.run(host=config['host'], port=config['port'], debug=True)
+    app.run(host=config['host'], port=config['port'])
